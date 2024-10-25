265f532401affa4d7f15ed3c22c0e247ba5095a4
gulshankapse <gulshankapse8@gmail.com>
2024-10-25 17:32:07 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import "./layout/layout.scss"
+import Navbar from "./componants/navbar/navbar.jsx"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/componants/navbar/navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./navbar.scss"
+
+function Navbar() {
+    const [open,setOpen] = useState(false);
+    return (
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                   <img src="/public/logoOLD.png" alt="img" />
+                   <span>HevenSeek</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contects</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">Sign In</a>
+                <a href="/" className="signup">Sign Up</a>
+                <div className="menuicon">
+                    <img src="public/menu.png" alt="img" onClick={()=>setOpen((prev)=>!prev)} />
+                </div>
+                <div className={open ? "menubar active" : "menubar"}>
+                  <a href="/">Home</a>
+                  <a href="/">About</a>
+                  <a href="/">Contects</a>
+                  <a href="/">Agents</a>
+                  <a href="/">Sign In</a>
+                  <a href="/">Sign Up</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
