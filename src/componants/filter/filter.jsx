8c2c1409e9f39ca519ff1d9151107043ca0dbdc6
gulshankapse <gulshankapse8@gmail.com>
2024-10-29 22:49:24 +0530

    filter update

--- src/componants/filter/filter.jsx
+++ src/componants/filter/filter.jsx
@@ -14,7 +14,7 @@ function Filter() {
             <div className="item">
                <label htmlFor="type">Type</label>
                <select name="type" id="type">
-                  <option value="">any</option>
+                  <option value="">select</option>
                   <option value="buy">Buy</option>
                   <option value="rent">Rent</option>
                </select>
@@ -22,7 +22,7 @@ function Filter() {
             <div className="item">
                <label htmlFor="property">Property</label>
                <select name="property" id="property">
-                  <option value="">any</option>
+                  <option value="">select</option>
                   <option value="apartment">Apartment</option>
                   <option value="house">House</option>
                   <option value="land">Land</option>
@@ -30,15 +30,15 @@ function Filter() {
             </div>
             <div className="item">
                <label htmlFor="minPrice">Min Price</label>
-               <input type="number" id="minPrice" name="minPrice" placeholder="any"/>
+               <input type="number" id="minPrice" name="minPrice" placeholder="0"/>
             </div>
             <div className="item">
                <label htmlFor="maxPrice">Max Price</label>
-               <input type="number" id="maxPrice" name="maxPrice" placeholder="any"/>
+               <input type="number" id="maxPrice" name="maxPrice" placeholder="0"/>
             </div>
             <div className="item">
                <label htmlFor="bedroom">Bedroom</label>
-               <input type="text" id="bedroom" name="bedroom" placeholder="any"/>
+               <input type="text" id="bedroom" name="bedroom" placeholder="enter"/>
             </div>
             <button>
                <img src="/search.png" alt="" />
